e5cd05eef44774fea10c41c5f2e07c50027f4303
Baimenov Timour <buchukiri@gmail.com>
2022-07-13 17:00:14 +0200

    plein de chose non terminé

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,36 @@
-                //VERSION MOBILE
+// const articles = {
+// id1 : {
+//         name : "Canard Jaune",
+//         prix : "11€95"
+//     },
+// id2 : {
+//         name : "CanardDragon",
+//         prix : "11€95"
+//     },
+// id3 : {
+//         name : "Canard Geek",
+//         prix : "11€95"
+//     },
+// id4 : {
+//         name : "Canard Licorne",
+//         prix : "11€95"
+//     },
+// id5 : {
+//         name : "Canard Bowie",
+//         prix : "11€95"
+//     }
+// }
+
+
+
+
+                    //VERSION MOBILE
 
 // 2 boutons in mediaqueries to switch next and prev
 const prevbtn = document.getElementById("pictures-prev")
 const nextbtn = document.getElementById("pictures-next")
-const imgXl = document.querySelector(".pictures-img")
+const imgXl = document.getElementById("main-art-img")
+
 
 const srcImgList = [
     "img/canard-jaune-1-l.png",
@@ -16,29 +43,77 @@ const srcImgList = [
 
 // clicks
 prevbtn.addEventListener("click",function (event) {
-   
-    imgXl.src="img/canard-jaune-5-l.png"
+    imgXl.src = srcImgList[4]
+    
 })
 
 
 nextbtn.addEventListener("click",function (event) {
-
-        imgXl.src ="img/canard-jaune-2-l.png"
+    imgXl.src = srcImgList[0]
+    nextbtn.addEventListener("click",function (e) {
+        imgXl.src = srcImgList[1]
+        nextbtn.addEventListener("click",function (event) {
+            imgXl.src = srcImgList[2]
+                nextbtn.addEventListener("click",function (event) {
+            imgXl.src = srcImgList[3]
+                nextbtn.addEventListener("click",function (event) {
+            imgXl.src = srcImgList[4]
+        })
+        })
+        })
+    })
+    
     });
 
 
 
 
                     //VERSION PC
-
-const ulThumbs = document.getElementById("thumbs")
-
+// mouseover
 
 
-ulThumbs.addEventListener("mouseover",function (event) {
-  
+
+// const thumbImg = document.querySelectorAll(".thumbs-img");
+//     thumbImg.forEach(img => {
+//         imgaddEventListener("mouseover", function (event) {
+//         console.log(this);
+//     });
     
-})
+//     })
+
+
+
+
+function mouseOver() {
+    const ulThumbs = document.getElementById("thumbs");
+    // console.log(ulThumbs);
+    const thumbs = document.querySelectorAll(".thumbs-itm");
+    // console.log(thumbs);
+    const thumbImg = document.querySelectorAll(".thumbs-img")
+    // console.log(thumbImg);
+
+
+
+    thumbImg.forEach(li => {
+        console.log(li);
+           
+    });
+    
+    // for (const img in li) {
+    //     li.addEventListener("mouseover",function (e){
+          
+    //     })
+    // }
+  
+    // for (const img in thumbs) {
+    //     img.addEventListener("mouseover",function (e) {
+    //         imgXl.src = this
+    //     })
+    // }
+
+}
+mouseOver()
+
 
 
                 // TOGLE BOUTONS
@@ -50,10 +125,52 @@ function toggleAvantages() {
 }
 function toggleCaracteristiques() {
         document.getElementById("caracteristiques-btn").addEventListener("click", function (event) {
+        document.getElementById("product-car").classList.toggle("hide-show");
 
-                document.getElementById("product-car").classList.toggle("hide-show");
-            })
-        }
+    })
+}
 
 toggleCaracteristiques()
 toggleAvantages()
+
+
+                // ADD TO CHART
+
+const cartList = "";
+
+// click
+const inputValue = document.getElementById("add-qty")
+const addBtn = document.getElementById("add-cta");
+
+// function add to cart
+function addCart() {
+    const spanNbmbr = document.querySelector(".cart-nb");
+        addBtn.addEventListener("click", function (event) {
+            spanNbmbr.innerHTML = inputValue.value;
+            addBtn.removeEventListener("click",addCart)
+            addBtn.innerHTML = "Déjà au panier"
+            addBtn.classList.add("add-cta-off")
+            
+            
+
+
+
+        // if (spanNbmbr.value >= 100) {
+        //     spanNbmbr.value.innerHTML = "+99";
+        // }
+
+
+        //    if (inputValue.value != 0) {
+        //         spanNbmbr.value ++
+        //    }
+
+        // if (document.querySelector(".cart-nb").value >= 100) {
+        //     document.querySelector(".cart-nb").innerHTML = inputValue.value;
+        // }
+       
+        
+
+ 
+})
+}
+addCart()
